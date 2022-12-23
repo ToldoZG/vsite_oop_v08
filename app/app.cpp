202bf38fd2822f09c78a95a07b503a900a4a0127
Nenad Čaklović <nenad.caklovic@reversinglabs.com>
2022-12-23 14:21:20 +0100

    solution

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,28 @@
 
 namespace vsite::oop::v8
 {
+int input_num(std::istream& is) {
+    int n; is >> n;
+    if (!is) throw not_number();
+    return n;
+}
+
+char input_op(std::istream& is) {
+    char c; is >> c;
+    if (c != '+' && c != '-' && c != '*' && c != '/')
+        throw not_operator();
+    return c;
+}
+
+double calc(int a, char c, int b) {
+    switch (c) {
+    case '+': return a + b;
+    case '-': return a - b;
+    case '*': return a * b;
+    case '/':
+        if (b == 0) throw divide_zero();
+        return static_cast<double>(a) / b;
+    }
+}
 
 }
--- app/app.h
+++ app/app.h
@@ -1,6 +1,36 @@
 #pragma once
-
+#include <iostream>
 namespace vsite::oop::v8 
 {
+    class base_exception {
+    public:
+        virtual std::string error() const = 0;
+        virtual ~base_exception() = default;
+    };
+
+    class not_number :public base_exception {
+    public:
+        std::string error() const override {
+            return "not a number";
+        }
+    };
+
+    class divide_zero :public base_exception {
+    public:
+        std::string error() const override {
+            return "divide by zero";
+        }
+    };
+
+    class not_operator : public base_exception {
+    public:
+        std::string error()  const override {
+            return "invalid operation";
+        }
+    };
+
+    int input_num(std::istream&);
+    char input_op(std::istream&);
+    double calc(int a, char c, int b);
 
 }
